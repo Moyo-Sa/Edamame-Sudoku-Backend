fb4cf5ab82400f001a2446dbb86c728f82711083
Moyo-Sa <128077572+Moyo-Sa@users.noreply.github.com>
2024-12-13 23:37:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,27 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+    'https://edamame-sudoku-moyo-sas-projects.vercel.app',
+    'https://edamame-sudoku-git-main-moyo-sas-projects.vercel.app',
+    'http://localhost:5173/'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Allow requests with no origin, like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/sudoku', router);
 
